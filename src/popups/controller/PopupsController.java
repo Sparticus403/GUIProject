22c2a6db6fb73c32ba973bf4c0460f10d3327c93
Sparticus403 <twil8980@740s-pl-332.local>
2016-09-27 09:52:12 -0600

    created loop for the controller

--- src/popups/controller/PopupsController.java
+++ src/popups/controller/PopupsController.java
@@ -1,10 +1,26 @@
 package popups.controller;
 
+import popups.view.PopupViewer;
+
 public class PopupsController
 {
+	private PopupViewer display;
+	
+	public PopupsController()
+	{
+		display = new PopupViewer();
+	}
 
 	public void start()
 	{
-		
+		String answer = "sample";
+		while(answer != null && !answer.equals(""))
+		{
+			display.displayMessage("Hi there :D");
+			
+			answer = display.collectResponse("what is going on?");
+			display.displayMessage(answer + " is what you said.");
+			
+		}
 	}
 }
