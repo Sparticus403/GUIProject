7e2416466f0612c07318946cd48832106b69e63f
Sparticus403 <twil8980@740s-pl-332.local>
2016-09-27 08:35:28 -0600

    Created a method to collect a response

--- src/popups/view/PopupViewer.java
+++ src/popups/view/PopupViewer.java
@@ -12,4 +12,16 @@ public class PopupViewer
 	{
 		JOptionPane.showMessageDialog(null, message);
 	}
+	/**
+	 * Creates an input popup to collect a response from the user as a String.
+	 * @param question The question being asked to the user.
+	 * @return The users response.
+	 */
+	public String collectResponse(String question)
+	{
+		String response = "";
+		response = JOptionPane.showInputDialog(null, question);
+		
+		return response;
+	}
 }
