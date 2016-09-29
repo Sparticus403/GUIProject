4a03e87000080ebb5bfd8c77395288c3daf7eae5
Sparticus403 <twil8980@740s-pl-332.local>
2016-09-29 08:25:34 -0600

    added a try/catch statement

--- src/popups/controller/PopupsController.java
+++ src/popups/controller/PopupsController.java
@@ -23,4 +23,27 @@ public class PopupsController
 			
 		}
 	}
+	/**
+	 * Checks if the supplied string can be parsed to a double value.
+	 * @param potentialValue The string to the test.
+	 * @return Whether the value could be parsed; true if it can be parsed, false otherwise.
+	 */
+	private boolean isDouble(String potentialValue)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			double test = Double.parseDouble(potentialValue);
+			isValid = true;
+		}
+		catch(NumberFormatException notDouble)
+		{
+			display.displayMessage("This is not a double value =:<");
+		}
+		
+		return isValid;
+		
+	}
+	
 }
