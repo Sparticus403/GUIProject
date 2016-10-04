062f8caf8d9ce5c4363806f9975a2244c221a8f6
Sparticus403 <twil8980@740s-pl-332.local>
2016-10-04 09:27:19 -0600

    added new methods to the array list

--- src/popups/controller/PopupsController.java
+++ src/popups/controller/PopupsController.java
@@ -18,6 +18,7 @@ public class PopupsController
 	public void start()
 	{
 		learnLists();
+		
 	}
 	private void testLoop()
 	{
@@ -77,6 +78,52 @@ public class PopupsController
 		Thingy testThingy = new Thingy();
 		thingyList.add(testThingy);
 		display.displayMessage(thingyList.size() + " is the size of the list.");
+		
+		for (int index = 0; index <5; index++)
+		{
+			Thingy addThingy = new Thingy();
+			thingyList.add(addThingy);
+			display.displayMessage(addThingy.getWords());
+		}
+		
+		display.displayMessage(thingyList.size() + " is the size");
+		
+		thingyList.get(3).setWords("look I changed a value");
+		Thingy temp = thingyList.get(4);
+		temp.setWords("stuff");
+		
+		for (int index = 0; index < thingyList.size(); index++)
+		{
+			display.displayMessage(thingyList.get(index).getWords());
+		}
+		
+		thingyList.add(3, new Thingy());
+		
+		display.displayMessage("The size is " + thingyList.size());
+		
+		thingyList.remove(0); //I no longer have access to this object. Because there is no = sign.
+		
+		display.displayMessage("The size is " + thingyList.size());
+		
+		Thingy deletedThingy = thingyList.remove(3);
+		display.displayMessage(deletedThingy.getWords() + " and the list size is " + thingyList.size());
+		
+		thingyList.set(0, deletedThingy);
+		
+		for (int index = 0; index < thingyList.size(); index++)
+		{
+			display.displayMessage(thingyList.get(index).getWords());
+		}
+		
+		for (Thingy currentThingy : thingyList)
+		{
+			display.displayMessage(currentThingy.getWords());
+		}
+		thingyList.get(2).setWords("Nothing Here?");
+		
+		Thingy thingDel = thingyList.remove(2);
+		display.displayMessage("This thing has been deleted: " + thingDel.getWords());
+		
 	}
 	
 }
